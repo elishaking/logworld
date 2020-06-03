fe70c7d5d5b7d314bff1e3ea5a29d26379743186
King Elisha <ek.chibueze@gmail.com>
2020-06-03 16:04:16 +0100

    calculate log

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,8 +1,11 @@
 import * as functions from "firebase-functions";
 
 export const calculateLog = functions.https.onRequest((req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  const body = JSON.parse(req.body);
+  const result = Math.log(body.argument) / Math.log(body.base);
   res.json({
     success: true,
-    result: 0,
+    result,
   });
 });
